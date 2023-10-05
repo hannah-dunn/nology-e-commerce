330dabf74b475c714627ddd034ee22b57065e144
Hannah <h.dunn@spin.net.au>
2023-10-05 14:21:44 +1100

    adds moving carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import styles from "./Carousel.module.scss";
 
 const CarouselPage = () => {
   return (
-    <Carousel>
+    <Carousel
+      autoPlay
+      interval="3000"
+      axis="horizontal"
+      infiniteLoop
+      className={styles.carousel}
+    >
       <div className="carouselpics">
         <img src="https://images.unsplash.com/photo-1597633125184-9fd7e54f0ff7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80" />
         {/* <p className="legend">Legend 1</p> */}
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,6 +13,13 @@ const NavBar = () => {
       </NavLink>
       <NavLink className={navLinkClasses} to="/cart">
         Cart
+        <img
+          className={styles.icon}
+          width="24"
+          height="24"
+          src="https://img.icons8.com/material-outlined/24/shopping-cart--v1.png"
+          alt="shopping-cart--v1"
+        />
       </NavLink>
     </nav>
   );
